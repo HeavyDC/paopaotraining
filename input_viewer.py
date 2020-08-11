71667ba651fb26edd91b21e59077bd325055a53b
Michael DA COSTA <penguin_power@hotmail.fr>
2020-08-11 14:03:02 +0200

    add requirements.txt file when issues faced

--- input_viewer.py
+++ input_viewer.py
@@ -1,15 +1,13 @@
 from __future__ import print_function
-
-
 from inputs import get_gamepad
 
 
 def main():
-    while 1:
+    print('Press Ctrl-break to quit.')
+    while True:
         events = get_gamepad()
         for event in events:
             print(event.code)
-
 
 if __name__ == "__main__":
     print("Test your inputs")
